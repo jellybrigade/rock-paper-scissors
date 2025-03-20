8fc667b5420f97c3b3f1c97cebaec9eed6266c5e
jellybrigade <schachnermarcel@protonmail.com>
2025-03-20 20:46:11 +0100

    Made the game playable.

--- main.js
+++ main.js
@@ -3,50 +3,44 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let humanChoice = prompt("Enter your choice: rock, paper, or scissors ");
     return humanChoice.toLowerCase();
 }
 
-let humanScore = 0
-let computerScore = 0
+let humanScore = 0;
+let computerScore = 0;
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
-
-function playRound(humanChoice, computerChoice){
-    if (humanChoice === computerChoice){
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
         console.log("It's a tie!");
-    }
-    else if (humanChoice === "rock" && computerChoice === "scissors"){
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
         console.log("You win! Rock crushes scissors.");
         humanScore++;
-    }
-    else if (humanChoice === "paper" && computerChoice === "rock"){
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You win! Paper covers rock.");
         humanScore++;
-    }
-    else if (humanChoice === "scissors" && computerChoice === "paper"){
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log("You win! Scissors cut paper.");
         humanScore++;
-    }
-    else {
+    } else {
         console.log("You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice + ".");
         computerScore++;
     }
 }
 
-function playGame(humanScore, computerScore){
-    while (humanScore < 3 && computerScore < 3){
+function playGame() {
+    while (humanScore < 3 && computerScore < 3) {
         let computerChoice = getComputerChoice();
         let humanChoice = getHumanChoice();
         playRound(humanChoice, computerChoice);
         console.log("Score: You " + humanScore + " - " + computerScore + " Computer");
     }
-    if (humanScore === 3){
+    if (humanScore === 3) {
         console.log("Congratulations! You win the game!");
-    }
-    else {
+    } else {
         console.log("Sorry! The computer wins the game.");
     }
 }
+
+playGame();
